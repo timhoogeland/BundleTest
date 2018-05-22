3e886fb16268ace12fd18c10087170b31666f906
MMooy <Michielmooy95@gmail.com>
2018-05-22 11:17:11 +0200

    baseDoaFix

--- src/main/java/Objects/Loan.java
+++ src/main/java/Objects/Loan.java
@@ -1,76 +1,77 @@
-package Objects;
+package bundlePWABackend.bundlePWABackend;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Loan {
-	private int loanId;
-	private int amount;
-	private String status;
-	private Date startDate;
-	private int duration;
-	private Date closingDate;
-	private String loanType;
-	private int contractId;
-	
-	public Loan(int loanId, int amount, String status, Date startDate, int duration, Date closingDate, String loanType, int contractId){
-		this.loanId = loanId;
-		this.amount = amount;
-		this.status = status;
-		this.startDate = startDate;
-		this.duration = duration;
-		this.closingDate = closingDate;
-		this.loanType = loanType;
-		this.contractId = contractId;
-	}
+private int loanId;
+private int amount;
+private String status;
+private String startdate;
+private int duration;
+private String closingdate;
+private String loantype;
+private int contractId;
 
-	public int getLoanId() {
-		return loanId;
-	}
-	public int getAmount() {
-		return amount;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public int getDuration() {
-		return duration;
-	}
-	public Date getClosingDate() {
-		return closingDate;
-	}
-	public String getLoanType() {
-		return loanType;
-	}
-	public int getContractId() {
-		return contractId;
-	}
-	public void setLoanId(int loanId) {
-		this.loanId = loanId;
-	}
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
-	public void setClosingDate(Date closingDate) {
-		this.closingDate = closingDate;
-	}
-	public void setLoanType(String loanType) {
-		this.loanType = loanType;
-	}
-	public void setContractId(int contractId) {
-		this.contractId = contractId;
-	}
-	
-	
+
+public Loan(int loanId, int amount, String status, String startdate, int duration, String closingdate, String loantype,int contractId) {
+	super();
+	this.loanId = loanId;
+	this.amount = amount;
+	this.status = status;
+	this.startdate = startdate;
+	this.duration = duration;
+	this.closingdate = closingdate;
+	this.loantype = loantype;
+	this.contractId = contractId;
+}
+public int getLoanId() {
+	return loanId;
+}
+public void setLoanId(int loanId) {
+	this.loanId = loanId;
+}
+public int getAmount() {
+	return amount;
+}
+public void setAmount(int amount) {
+	this.amount = amount;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public String getStartdate() {
+	return startdate;
+}
+public void setStartdate(String startdate) {
+	this.startdate = startdate;
+}
+public int getDuration() {
+	return duration;
+}
+public void setDuration(int duration) {
+	this.duration = duration;
+}
+public String getClosingdate() {
+	return closingdate;
+}
+public void setClosingdate(String closingdate) {
+	this.closingdate = closingdate;
+}
+public String getLoantype() {
+	return loantype;
+}
+public void setLoantype(String loantype) {
+	this.loantype = loantype;
+}
+public int getcontractId() {
+	return contractId;
+}
+public void setcontractId(int contractId) {
+	this.contractId = contractId;
+}
+
+
 }
