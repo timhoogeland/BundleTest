26e8094de60799e258473e5ef689429f09a6c6b2
Niels van Brakel <https://NielsvanBrakel@bitbucket.org/MinoxTeam/rysst-repo.git>
2018-05-29 13:32:58 +0200

    fixed bug

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -170,8 +170,6 @@ function getContracts() {
 	hr.send(null);
 }
 
-<<<<<<< HEAD
-=======
 function getUser() {
 	var hr = new XMLHttpRequest();
 	var id = undefined;
@@ -216,7 +214,6 @@ function checkValue(value){
 	
 	return value;
 }
->>>>>>> cacb379b39eadd57b8236edde10bac275768367b
 
 function toEditLoan(loanid) {
 	window.location.replace("edit_loan.jsp?id=" + loanid);
@@ -231,93 +228,7 @@ function UCFirst(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-<<<<<<< HEAD
-function loadLoanDetails() {
-	var hr = new XMLHttpRequest();
-
-	hr.open("GET", "/bundlePWABackend/restservices/loan/"
-			+ getParameterByName('id'), true);
-
-	hr.onreadystatechange = function() {
-		if (hr.readyState == 4 && hr.status == 200) {
-			var data = JSON.parse(hr.responseText);
-			$('#status').text(UCFirst(data[0].status));
-			$('#remaining').text("TODO");
-			$('#loantype').text(UCFirst(data[0].loantype));
-			$('#remainingbar').attr('value', 0);
-			$('#remainingbar').attr('max', data[0].amount);
-			$('#amount').text("$ " + data[0].amount);
-			$('#duration').text(data[0].duration);
-			$('#startdate').text(data[0].startdate);
-			$('#closingdate').text(data[0].closingdate);
-
-			var hr2 = new XMLHttpRequest();
-			hr2.open("GET", "/bundlePWABackend/restservices/contract/"
-					+ data[0].contractid, true);
-
-			hr2.onreadystatechange = function() {
-				if (hr2.readyState == 4 && hr2.status == 200) {
-					var data = JSON.parse(hr2.responseText);
-
-					var hr3 = new XMLHttpRequest();
-					hr3.open("GET", "/bundlePWABackend/restservices/user/"
-							+ data[0].useridfk, true);
-
-					hr3.onreadystatechange = function() {
-						if (hr3.readyState == 4 && hr3.status == 200) {
-							var data = JSON.parse(hr3.responseText);
-							$('#loanName').text(
-									'Loan - ' + UCFirst(data[0].name));
-							$('#name').text(UCFirst(data[0].name));
-							;
-							$('#dateofbirth').text(data[0].dateofbirth);
-							$('#phone').text(data[0].phonenumber);
-							$('#role').text(data[0].userType);
-							$('#userstatus').text(data[0].status);
-
-							var hr4 = new XMLHttpRequest();
-							hr4.open("GET",
-									"/bundlePWABackend/restservices/adress/"
-											+ data[0].adresIDFK, true);
-
-							hr4.onreadystatechange = function() {
-								if (hr4.readyState == 4 && hr4.status == 200) {
-									var data = JSON.parse(hr4.responseText);
-									$('#street').text(
-											data[0].street + " "
-													+ data[0].number);
-									$('#postal').text(data[0].postalcode);
-									$('#country').text(data[0].country);
-								} else if (hr4.readyState == 4) {
-									addNotification('Retrieving data failed with status '
-											+ hr4.status + '. Try again later.');
-								}
-							}
-							hr4.send(null);
-						} else if (hr3.readyState == 4) {
-							addNotification('Retrieving data failed with status '
-									+ hr3.status + '. Try again later.');
-						}
-					}
-					hr3.send(null);
-
-				} else if (hr2.readyState == 4) {
-					addNotification('Retrieving data failed with status ' + hr2.status
-							+ '. Try again later.');
-				}
-
-			}
-			hr2.send(null);
-
-		} else if (hr.readyState == 4) {
-			addNotification('Retrieving data failed with status ' + hr.status
-					+ '. Try again later.');
-		}
-	}
-	hr.send(null);
-}
-
-function getGroups{
+function getGroups() {
   var hr = new XMLHttpRequest();
   hr.open("GET", "/bundlePWABackend/restservices/loan", true);
 
@@ -353,10 +264,6 @@ function getGroups{
   hr.send(null);
 }
 
-}
-
-=======
->>>>>>> cacb379b39eadd57b8236edde10bac275768367b
 function getParameterByName(name, url) {
 	if (!url)
 		url = window.location.href;
