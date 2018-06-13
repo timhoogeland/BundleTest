d5c1df44b95f2f92e1d1dbe138049ba21fe60cd0
Tim <stellager@users.noreply.github.com>
2018-06-13 14:20:38 +0200

    nee

--- src/main/java/Resource/PdfResource.java
+++ src/main/java/Resource/PdfResource.java
@@ -44,8 +44,8 @@ public class PdfResource {
 							 @FormParam("useridfk") String userIdFk) throws IOException, DocumentException{
 		File file = File.createTempFile("contract_" + userIdFk, ".pdf");
 	
-		PdfReader reader = new PdfReader("/target/test1.2.pdf");
-		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(new File("//target//"+file.getName())));
+		PdfReader reader = new PdfReader("test1.2.pdf");
+		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(new File(file.getName())));
 		AcroFields form = stamper.getAcroFields();
 		form.setField("firstname",firstname );
 		form.setField("lastname", lastname);
